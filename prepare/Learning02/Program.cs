37d92dd14a8af3c2a5af5991cdc41dee84c87907
elizabethcolindres7 <elizabethcolindres2072003@hotmail.es>
2024-04-29 21:03:26 -0600

    new file:   prepare/Learning02/Job.cs modified:   prepare/Learning02/Program.cs new file:   prepare/Learning02/Resume.cs

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+            Job job1 = new Job();
+            job1._jobTitle = "Software Engineer";
+            job1._company = "Telus";
+            job1._startYear = 2021;
+            job1._endYear = 2024;
+            
+
+            Job job2 = new Job();
+            job2._jobTitle = "Supervisor";
+            job2._company = "Teleperformance";
+            job2._startYear = 2015;
+            job2._endYear = 2020;
+            
+
+            Resume myResume = new Resume();
+            myResume._name = "Taylor Cox";
+
+            myResume._jobs.Add(job1);
+            myResume._jobs.Add(job2);
+
+            myResume.Display();
     }
 }
