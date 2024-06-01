64a1270295540233c39cfa16403bf34c4a3dd05f
elizabethcolindres7 <elizabethcolindres2072003@hotmail.es>
2024-06-01 13:26:34 -0600

    modified:   final/Foundation1/Video.cs

--- final/Foundation1/Video.cs
+++ final/Foundation1/Video.cs
@@ -3,28 +3,28 @@ public class Video
     public string Title { get; set; }
     public string Author { get; set; }
     public int LengthInSeconds { get; set; }
-    private List<Comment> Comments { get; set; }
+    private List<Comment> _comments;
 
     public Video(string title, string author, int lengthInSeconds)
     {
         Title = title;
         Author = author;
         LengthInSeconds = lengthInSeconds;
-        Comments = new List<Comment>();
+        _comments = new List<Comment>();
     }
 
     public void AddComment(Comment comment)
     {
-        Comments.Add(comment);
+        _comments.Add(comment);
     }
 
     public int GetNumberOfComments()
     {
-        return Comments.Count;
+        return _comments.Count;
     }
 
     public List<Comment> GetComments()
     {
-        return Comments;
+        return _comments;
     }
 }
